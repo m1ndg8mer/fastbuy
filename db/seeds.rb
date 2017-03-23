1375acb36bf9633f27ba912a16fd19f99ce64bca
Roman <roman0tershak@gmail.com>
2017-03-23 02:36:22 +0200

    Hotfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,4 @@ admin.add_role :admin
 admin.save
 
 Category.create!(title: 'Mobile Phones')
+Product.create!(title: 'Nokia 1100', qty: 15, price: 17.99, description: 'The best phone', category_id: Category.first.id)
